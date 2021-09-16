88db05feaad1d923073060ae20e5ba20013a76e3
Zihan Zheng <819932926@qq.com>
2021-09-16 22:39:10 +0800

    add spirits of pac man game

--- Assets/Script/AudioController.cs
+++ Assets/Script/AudioController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
+    [SerializeField]
+    private AudioSource source;
+    public AudioClip[] audioClips = new AudioClip[8];
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("musicPlay");
     }
 
     // Update is called once per frame
@@ -15,4 +18,14 @@ public class AudioController : MonoBehaviour
     {
         
     }
+
+    IEnumerator musicPlay()
+    {
+        source.clip = audioClips[0];
+        source.Play();
+        yield return new WaitForSeconds(source.clip.length);
+        source.clip = audioClips[1];
+        source.Play();
+    }
+
 }
